27600e8a837edfc957952288ade645b48cb927a6
Michelle Sagnelli <ing.sagnelli@gmail.com>
2017-11-03 07:27:57 -0600

    Added solution to project, and finished register and login functionality

---
+++ Wubbalubbadubzork/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Wubbalubbadubzork.Models;
+using Microsoft.AspNet.Identity.Owin;
+
+namespace Wubbalubbadubzork.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        public ActionResult Index()
+        {
+            if (Request.IsAuthenticated)
+            {
+                var userId = User.Identity.GetUserId();
+                var manager = HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+                var user = manager.FindById(userId);
+                ViewBag.Name = user.Name;
+            }
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            if (Request.IsAuthenticated)
+            {
+                var userId = User.Identity.GetUserId();
+                var manager = HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+                var user = manager.FindById(userId);
+                ViewBag.Name = user.Name;
+            }
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            if (Request.IsAuthenticated)
+            {
+                var userId = User.Identity.GetUserId();
+                var manager = HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+                var user = manager.FindById(userId);
+                ViewBag.Name = user.Name;
+            }
+            return View();
+        }
+    }
+}
