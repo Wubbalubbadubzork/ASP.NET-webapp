fc2e6e924416e2849716a286e51b5b557acd2333
Michelle Sagnelli <ing.sagnelli@gmail.com>
2017-11-14 06:31:12 -0600

    Finished??

--- Wubbalubbadubzork/Content/Javascripts/GameHub.js
+++ Wubbalubbadubzork/Content/Javascripts/GameHub.js
@@ -14,7 +14,65 @@
     }
     gameHub.client.rollDice = function (number) {
         $('#dice').html(number);
+        $('#button_dice').prop('disabled', true);
+        gameHub.server.establishOptions($("#gameId").html().trim(), parseInt($("#characterTurnId").html().trim(), 10), parseInt($("#sceneId").html().trim(), 10));
     }
+
+    gameHub.client.setSkills = function (id, i, name, targetid, type) {
+        for (var j = 1; j < i; j++) {
+            $("#skill" + i).html(name);
+            $("#skill" + i).prop('disabled', false);
+            $("#skill" + i).click(function () {
+                gameHub.server.executeSkill($("#gameId").html().trim(), parseInt($("#sceneId").html().trim(), 10), parseInt($("#characterTurnId").html().trim(), 10), id, name, targetid, type, parseInt($("#dice").html().trim()));
+            })
+        }
+    }
+
+    gameHub.client.setOption = function (i, name, tag) {
+        $("#option" + i).html(name);
+        if (name == "Si mismo") {
+            $("#option" + i).addClass('selfOption');
+            $("#option" + i).click(function () {
+                gameHub.server.establishSkills($("#gameId").html().trim(), parseInt($("#characterTurnId").html().trim(), 10), name,'selfOption');
+            })
+        }
+        else if (name == "Todos enemigos") {
+            $("#option" + i).addClass('enemiesOption');
+            $("#option" + i).click(function () {
+                gameHub.server.establishSkills($("#gameId").html().trim(), parseInt($("#characterTurnId").html().trim(), 10), name,'enemiesOption');
+            })
+        }
+        else if (name == "Todos aliados") {
+            $("#option" + i).addClass('alliesOption');
+            $("#option" + i).click(function () {
+                gameHub.server.establishSkills($("#gameId").html().trim(), parseInt($("#characterTurnId").html().trim(), 10), name,'alliesOption');
+            })
+        }
+        else {
+            if (name == "Darius" || name == "Veigar" || name == "Ashe") {
+                $("#option" + i).addClass(tag + "ally");
+                $("#option" + i).click(function () {
+                    gameHub.server.establishSkills($("#gameId").html().trim(), parseInt($("#characterTurnId").html().trim(), 10), name, tag + "ally");
+                })
+            }
+            else {
+                $("#option" + i).addClass(tag);
+                $("#option" + i).click(function () {
+                    gameHub.server.establishSkills($("#gameId").html().trim(), parseInt($("#characterTurnId").html().trim(), 10), name, tag);
+                })
+            }
+        }
+        $("#option" + i).prop('disabled', false);       
+    }
+
+    gameHub.client.whoIsNext = function () {
+        gameHub.server.NextTurn($("#gameId").html().trim(), parseInt($("#sceneId").html().trim(), 10), parseInt($("#characterTurnId").html().trim(), 10));
+    }
+
+    gameHub.client.runTurn = function () {
+        gameHub.server.Turn($("#gameId").html().trim(), parseInt($("#characterTurnId").html().trim(), 10));
+    }
+
     gameHub.client.printScene = function (username, message) {
         $('#mainServer').append('<li>' + username + ': ' + message + '</li>');
     }
@@ -27,6 +85,31 @@
         })
     }
 
+    gameHub.client.isTurn = function (id, playable) {
+        $("#characterTurnId").html(id);
+        $("#playableOrNot").html(playable);
+    }
+
+    gameHub.client.announceTurn = function () {
+        alert("It is your turn boy. Please roll the dice.");
+        $("#button_dice").prop('disabled', false);
+        for (var i = 1; i <= 7; i++) {
+            if (i <= 4) {
+                $("#option" + i).prop('disabled', true);
+                $("#skill" + i).prop('disabled', true);
+            }
+        }
+        gameHub.server.sceneDisplay("Your turn. Please roll the dice", $("#gameId").html().trim());
+    }
+
+    gameHub.client.playerTurn = function () {
+        gameHub.server.playerTurn($("#gameId").html().trim(), parseInt($("#characterTurnId").html().trim(), 10));
+    }
+
+    gameHub.client.enemyTurn = function () {
+        gameHub.server.enemyTurn($("#gameId").html().trim(), parseInt($("#characterTurnId").html().trim(), 10));
+    }
+
     $.connection.hub.start().done(function () {
         console.log('Now connected');
         gameHub.server.establishCharacters($("#gameId").html().trim());
@@ -36,19 +119,22 @@
             scene_id: $("#sceneId").html().trim()
         }).done(function (response) {
             gameHub.server.sceneDisplay(response, $("#gameId").html().trim());
-        })
+        });
+
+        gameHub.server.whoseTurn($("#gameId").html().trim());
+        gameHub.server.turn($("#gameId").html().trim(), parseInt($("#characterTurnId").html().trim(),10));
 
         $("#send").click(function () {
             if ($("#message").val() != '') {
                 gameHub.server.sendMessage($('#username').html().trim(), $("#message").val(), $("#gameId").html().trim());
                 $("#message").val('');
             }
-        })()
+        });
 
         $("#button_dice").click(function () {
             var result = dice.roll();
             gameHub.server.printDice(result, $("#gameId").html().trim());
-        })
+        });
     }).fail(function () {
         console.log('Did not work');
     })
--- Wubbalubbadubzork/Hubs/GameHub.cs
+++ Wubbalubbadubzork/Hubs/GameHub.cs
@@ -38,6 +38,239 @@ namespace Wubbalubbadubzork.Hubs
             }
         }
 
+        public void WhoseTurn(string gameId)
+        {
+            var keys = Characters[gameId].Keys;
+            foreach(var i in keys)
+            {
+                if(Characters[gameId][i].Is_Turn == true)
+                {
+                    Clients.Group(gameId).isTurn(Characters[gameId][i].Id, Characters[gameId][i].Playable);
+                    break;
+                }
+            }
+        }
+
+        public void NextTurn(string gameId, int scene_id, int currentTurn)
+        {
+            var keys = Characters[gameId].Keys.ToList();
+            Random rnd = new Random();
+            int random = rnd.Next(0, keys.Count);
+            var id = keys[random];
+
+            if(Characters[gameId][id].Health > 0)
+            {
+                if(Characters[gameId][id].Scene_Id == scene_id)
+                {
+                    Characters[gameId][currentTurn].Is_Turn = false;
+                    Characters[gameId][id].Is_Turn = true;
+                }
+            }
+
+            Clients.Group(gameId).isTurn(Characters[gameId][id].Id, Characters[gameId][id].Playable);
+            Clients.Group(gameId).runTurn();
+        }
+
+        public void Turn(string gameId, int characterId)
+        {
+            var character = db.Characters.Find(characterId);
+            if (character.Playable == true)
+            {
+                Clients.Group(gameId).printScene("Narrador ", "Es turno de: " + character.Name);
+                Clients.Group(gameId).playerTurn();
+            }
+            else
+            {
+                Clients.Group(gameId).printScene("Narrador ", "Es turno de: " + character.Name);
+                Clients.Group(gameId).enemyTurn();
+            }
+
+        }
+
+        public void EnemyTurn(string gameId)
+        {
+            Clients.Group(gameId).printScene("Narrador ", "Not implemented yet.");
+            Clients.Group(gameId).whoIsNext();
+        }
+
+        public void ExecuteSkill(string gameId, int scene_id, int characterId, int skillId, string skillName, string targetId, string type, int dice)
+        {
+            var character = Characters[gameId][characterId];
+            var connectionId = User_Characters[gameId][character];
+            var target = new List<Character>();
+            if (targetId == "selfOption")
+            {
+                target.Add(character);
+            }
+            else if(targetId == "enemiesOption")
+            {
+                var enemies_in_scene = db.Characters.Where(u => u.Scene_Id == scene_id).ToList();
+                target = new List<Character>();
+                var i = 1;
+                foreach (var j in enemies_in_scene)
+                {
+                    if (Characters[gameId][j.Id].Health > 0)
+                    {
+                        target.Add(Characters[gameId][j.Id]);
+                    }
+                }
+            }
+            else if(targetId == "alliesOption")
+            {
+                 target = Characters[gameId].Values.Where(u => u.Playable == true).ToList();
+            }
+            else if (targetId.EndsWith("ally"))
+            {
+                target.Add(Characters[gameId][Int32.Parse(targetId.Substring(0, 1))]);
+            }
+            else
+            {
+                target.Add(Characters[gameId][Int32.Parse(targetId)]);
+            }
+
+            if(type == "damage")
+            {
+                foreach(var j in target)
+                {
+                    j.Health = j.Health - db.Skills.Find(skillId).Base_Power + dice;
+                    Clients.Group(gameId).printScene("Narrador ", j.Name + " perdio " + db.Skills.Find(skillId).Base_Power + dice + " puntos de vida, ahora tiene: " + j.Health);
+                }
+            }
+            else
+            {
+                foreach (var j in target)
+                {
+                    j.Health = j.Health + db.Skills.Find(skillId).Base_Power + dice;
+                    Clients.Group(gameId).printScene("Narrador ", j.Name + " gano " + db.Skills.Find(skillId).Base_Power + dice + " puntos de vida, ahora tiene: " + j.Health);
+                }
+            }
+
+            Clients.Group(gameId).whoIsNext();
+        }
+
+        public void EstablishSkills(string gameId, int characterId, string name, string targetId)
+        {
+            var character = Characters[gameId][characterId];
+            var connectionId = User_Characters[gameId][character];
+            if(character.Name == "Darius")
+            {
+                if(name == "Si mismo")
+                {
+                    var target = character;
+                    Clients.Client(connectionId).setSkills(3, 1, db.Skills.Find(3).Name, targetId, "cure");
+                }
+                else
+                {
+                    var id = Int32.Parse(targetId);
+                    var target = Characters[gameId][id];
+                    for(int i = 1; i <= 2; i++)
+                    {
+                        Clients.Client(connectionId).setSkills(i, i, db.Skills.Find(i).Name, targetId, "damage");
+                    }
+                }
+            }
+            if (character.Name == "Veigar")
+            {
+                if(name == "Todos enemigos")
+                {
+                    Clients.Client(connectionId).setSkills(5, 1, db.Skills.Find(5).Name, targetId, "damage");
+                }
+                else
+                {
+                    Clients.Client(connectionId).setSkills(4, 1, db.Skills.Find(4).Name, targetId, "damage");
+                    Clients.Client(connectionId).setSkills(6, 1, db.Skills.Find(6).Name, targetId, "damage");
+                }
+            }
+            if (character.Name == "Ashe")
+            {
+                if (name == "Todos enemigos")
+                {
+                    Clients.Client(connectionId).setSkills(8, 1, db.Skills.Find(8).Name, targetId, "damage");
+                }
+                else if(name == "Todos aliados")
+                {
+                    Clients.Client(connectionId).setSkills(9, 1, db.Skills.Find(9).Name, targetId, "cure");
+                }
+                else
+                {
+                    Clients.Client(connectionId).setSkills(7, 1, db.Skills.Find(9).Name, targetId, "damage");
+                }
+            }
+            if (character.Name == "Torin")
+            {
+                if (name == "Si mismo")
+                {
+                    var target = character;
+                    Clients.Client(connectionId).setSkills(3, 1, db.Skills.Find(3).Name, targetId, "cure");
+                }
+                if (name == "Todos enemigos")
+                {
+                    Clients.Client(connectionId).setSkills(8, 1, db.Skills.Find(8).Name, targetId, "damage");
+                }
+                else if (targetId.EndsWith("ally"))
+                {
+                    Clients.Client(connectionId).setSkills(9, 1, db.Skills.Find(9).Name, targetId, "cure");
+                }
+                else
+                {
+                    Clients.Client(connectionId).setSkills(7, 1, db.Skills.Find(9).Name, targetId, "damage");
+                }
+            }
+        }
+
+        public void EstablishOptions(string gameId, int characterId, int scene_id)
+        {
+            var character = Characters[gameId][characterId];
+            var connectionId = User_Characters[gameId][character];
+            var enemies_in_scene = db.Characters.Where(u => u.Scene_Id == scene_id).ToList();
+            var i = 1;
+            foreach (var j in enemies_in_scene)
+            {
+                if (Characters[gameId][j.Id].Health > 0)
+                {
+                    Clients.Client(connectionId).setOption(i, j.Name, j.Id);
+                    i++;
+                }
+            }
+            if (character.Name == "Darius")
+            {
+                Clients.Client(connectionId).setOption(i, "Si mismo", "Si mismo");
+            }
+            else if (character.Name == "Veigar")
+            {
+                Clients.Client(connectionId).setOption(i, "Todos enemigos", "Todos enemigos");
+            }
+            else if (character.Name == "Ashe")
+            {
+                Clients.Client(connectionId).setOption(i, "Todos enemigos", "Todos enemigos");
+                i++;
+                Clients.Client(connectionId).setOption(i, "Todos aliados", "Todos aliados");
+            }
+            else if (character.Name == "Torin")
+            {
+                Clients.Client(connectionId).setOption(i, "Todos enemigos", "Todos enemigos");
+                i++;
+                Clients.Client(connectionId).setOption(i, "Darius", db.Characters.Where(u=>u.Name == "Darius").FirstOrDefault().Id);
+                i++;
+                Clients.Client(connectionId).setOption(i, "Veigar", db.Characters.Where(u => u.Name == "Veigar").FirstOrDefault().Id);
+                i++;
+                Clients.Client(connectionId).setOption(i, "Ashe", db.Characters.Where(u => u.Name == "Ashe").FirstOrDefault().Id);
+                i++;
+                Clients.Client(connectionId).setOption(i, "Si mismo", "Si mismo");
+
+            }
+        }
+
+        public void PlayerTurn(string gameId, int characterId)
+        {
+            if (Characters[gameId][characterId].Health > 0)
+            {
+                var character = Characters[gameId][characterId];
+                var connectionId = User_Characters[gameId][character];
+                Clients.Client(connectionId).announceTurn();
+            }
+        }
+
         public void EstablishCharacters(string gameId)
         {
             var All_Characters = db.Characters.OrderBy(u => u.Id).ToList();
@@ -78,7 +311,7 @@ namespace Wubbalubbadubzork.Hubs
 
         public void SceneDisplay(string description, string gameId)
         {
-            Clients.Group(gameId).printScene("Narrador: ", description);
+            Clients.Group(gameId).printScene("Narrador", description);
         }
 
         public void SendMessage(string username, string message, string gameId)
